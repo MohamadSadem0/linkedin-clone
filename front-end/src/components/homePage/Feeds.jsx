160e1f387e83b361c05b208690b4d6566b94b889
MohamadSadem0 <mohamad1.sadem@gmail.com>
2024-03-25 16:15:04 +0200

    feeds half

---
+++ front-end/src/components/homePage/Feeds.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Feeds = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get("");
+        setPosts(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchPosts();
+  }, []); 
+
+  return (
+    <div>
+      {posts.map(({ username, description, image, likes }, index) => (
+        <div key={index} className="feed">
+          <div className="username">{username}</div>
+          <div className="post-description">{description}</div>
+          <img className="post-img" src={image} alt="Post" />
+          <div className="likes">Likes: {likes}</div>
+          <div>
+            <ul>
+              <li>like</li>
+              <li>comment</li>
+              <li>repost</li>
+              <li>send</li>
+            </ul>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Feeds;
